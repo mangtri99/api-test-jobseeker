797809e9dbdff650e7a76c66013ad8bfa4b82aa5
Mang Tri <mangtri93@gmail.com>
2023-11-04 22:55:01 +0800

    finishing

--- app/Models/Candidate.php
+++ app/Models/Candidate.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Candidate extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     // table t_candidate
     protected $table = 't_candidate';
--- database/migrations/2023_11_03_072304_create_candidates_table.php
+++ database/migrations/2023_11_03_072304_create_candidates_table.php
@@ -22,7 +22,6 @@ return new class extends Migration
             $table->string('year_exp', 5);
             $table->string('last_salary', 100)->nullable();
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
